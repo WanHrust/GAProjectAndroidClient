1636523af1a8f93f21ca16c2d20be3a565775d68
Rustam Alashrafov <rustam.alashrafov@gmail.com>
2016-03-27 16:57:36 +0200

    Registers with GCM regid

--- app/src/main/java/com/example/rustam/gaprojectandroidclient/RegisterActivity.java
+++ app/src/main/java/com/example/rustam/gaprojectandroidclient/RegisterActivity.java
@@ -12,12 +12,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.os.AsyncTask;
+
+import com.google.android.gms.gcm.GoogleCloudMessaging;
 
 import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,8 +30,12 @@ public class RegisterActivity extends Activity {
     EditText email, password, firstname, lastname, city, street;
     Button login, register;
     String emailtxt, passwordtxt, firstnametxt, lastnametxt, citytxt, streettxt;
-    List<NameValuePair> params;
+    List<NameValuePair> jsonparams;
     String serverName;
+
+    GoogleCloudMessaging gcm;
+    String regid;
+    String PROJECT_NUMBER = "310394170924";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,16 +71,49 @@ public class RegisterActivity extends Activity {
                 lastnametxt = lastname.getText().toString();
                 citytxt = city.getText().toString();
                 streettxt = street.getText().toString();
-                params = new ArrayList<NameValuePair>();
-                params.add(new BasicNameValuePair("email", emailtxt));
-                params.add(new BasicNameValuePair("password", passwordtxt));
-                params.add(new BasicNameValuePair("firstname", firstnametxt));
-                params.add(new BasicNameValuePair("lastname", lastnametxt));
-                params.add(new BasicNameValuePair("city", citytxt));
-                params.add(new BasicNameValuePair("street", streettxt));
+                jsonparams = new ArrayList<NameValuePair>();
+                getRegId();
 
+
+            }
+        });
+    }
+
+    public void getRegId() {
+        new AsyncTask<Void, Void, String>() {
+            @Override
+            protected String doInBackground(Void... params) {
+                String msg = "";
+                try {
+                    if (gcm == null) {
+                        gcm = GoogleCloudMessaging.getInstance(getApplicationContext());
+                    }
+                    regid = gcm.register(PROJECT_NUMBER);
+
+                    msg = "Device registered, registration ID=" + regid;
+
+                    Log.i("GCM", msg);
+
+                } catch (IOException ex) {
+                    msg = "Error :" + ex.getMessage();
+
+                }
+                return msg;
+            }
+
+            @Override
+            protected void onPostExecute(String msg) {
+                //etRegId.setText(msg + "\n");
+                jsonparams.add(new BasicNameValuePair("email", emailtxt));
+                jsonparams.add(new BasicNameValuePair("password", passwordtxt));
+                jsonparams.add(new BasicNameValuePair("firstname", firstnametxt));
+                jsonparams.add(new BasicNameValuePair("lastname", lastnametxt));
+
+                jsonparams.add(new BasicNameValuePair("street", streettxt));
+                jsonparams.add(new BasicNameValuePair("regid", regid)); // not sure if its a good prace
+                jsonparams.add(new BasicNameValuePair("city", citytxt));
                 ServerRequest sr = new ServerRequest();
-                JSONObject json = sr.getJSON(serverName + "register", params);
+                JSONObject json = sr.getJSON(serverName + "register", jsonparams);
                 //JSONObject json = sr.getJSON("http://192.168.56.1:8080/register",params);
 
                 if (json != null) {
@@ -87,8 +128,7 @@ public class RegisterActivity extends Activity {
                     }
                 }
             }
-        });
+        }.execute(null, null, null);
     }
-
 
 }
